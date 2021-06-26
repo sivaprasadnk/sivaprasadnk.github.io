4a1bf072e71cca2442db8e0ba2e2ff7965560010
sivaprasadnk <75713903+sivaprasadnk@users.noreply.github.com>
2021-06-26 17:54:56 +0530

    correcting description

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -37,8 +37,8 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "558f26363468c5944feaddc4fefce9ee",
-"/": "558f26363468c5944feaddc4fefce9ee",
+"index.html": "327e1ea558c7f1f652bbf90394ec9452",
+"/": "327e1ea558c7f1f652bbf90394ec9452",
 "main.dart.js": "a82b5fe93b634f820ddeaf4afc19f559",
 "manifest.json": "c5f77348de3d65f8126c73b9bce93f8c",
 "version.json": "ecbaabfffdf2314297fcf6fb6e9d7ca8"
