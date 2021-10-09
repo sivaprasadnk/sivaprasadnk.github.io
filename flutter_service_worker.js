4d5d07e5f2d091429fb1f4ce546aaff41f0181a2
sp-sivaprasad <sivaprasad@indbytes.com>
2021-10-09 08:22:09 +0530

    correction for ios

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -37,9 +37,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "3cbd22282fc3da8d6846c3e79fb8636c",
-"/": "3cbd22282fc3da8d6846c3e79fb8636c",
-"main.dart.js": "546afc968f43231bea0997ddb8b4ca48",
+"index.html": "0040030c692ede146cc696d2a8e821da",
+"/": "0040030c692ede146cc696d2a8e821da",
+"main.dart.js": "cf17f5a616f696c2e013ca22f3edbbee",
 "manifest.json": "c5f77348de3d65f8126c73b9bce93f8c",
 "version.json": "ecbaabfffdf2314297fcf6fb6e9d7ca8"
 };
