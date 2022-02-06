c28fe4b0e40084ab95c79d8f9d6174cd4643181a
sp-sivaprasad <sivaprasad@indbytes.com>
2022-02-06 11:01:23 +0530

    adding android footer

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -86,9 +86,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "ec7fc93a976e0533fa28a5d3c78c20bb",
-"/": "ec7fc93a976e0533fa28a5d3c78c20bb",
-"main.dart.js": "96aa2bae541bdd24aa18ce4a31169848",
+"index.html": "6409e639d277a2da301a27e88c23d498",
+"/": "6409e639d277a2da301a27e88c23d498",
+"main.dart.js": "4b9fb9ec892722dd93036512e82b75e7",
 "manifest.json": "c5f77348de3d65f8126c73b9bce93f8c",
 "version.json": "5f96fc1e25eaa46bce39acfde46493d7"
 };
