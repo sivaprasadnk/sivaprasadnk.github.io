4e888a91eb6099d01c82676acb646f3d72d9daf1
sp-sivaprasad <sivaprasad@indbytes.com>
2022-02-06 20:46:20 +0530

    build command  correction

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -86,9 +86,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "6409e639d277a2da301a27e88c23d498",
-"/": "6409e639d277a2da301a27e88c23d498",
-"main.dart.js": "4b9fb9ec892722dd93036512e82b75e7",
+"index.html": "ef34c84bf49dbea2ddc63f4733617d75",
+"/": "ef34c84bf49dbea2ddc63f4733617d75",
+"main.dart.js": "60fcd26de2f6a34f7071ab2c5b2c7883",
 "manifest.json": "c5f77348de3d65f8126c73b9bce93f8c",
 "version.json": "5f96fc1e25eaa46bce39acfde46493d7"
 };
