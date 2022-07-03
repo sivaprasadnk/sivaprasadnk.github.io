892802a6096dab9da0c995305e3f1b6f6a794da0
sivaprasadnk <sivaprasadnk123@gmail.com>
2022-07-03 21:00:12 +0530

    adding privacy policy

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -93,10 +93,12 @@ const RESOURCES = {
 "icons/Icon-192.png": "11cf43083c086a6822d0d71a849a78d7",
 "icons/Icon-192a.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "ce4c16452f3da86f09ad5bddb983e6bb",
-"/": "ce4c16452f3da86f09ad5bddb983e6bb",
+"index.html": "62ebd20699b0256f28d9a0f45a540584",
+"/": "763bd168c5922aea9b77795ff9638bfe",
 "main.dart.js": "345eca65dd2da5e6f7b09b631ccb8e2a",
 "manifest.json": "c5f77348de3d65f8126c73b9bce93f8c",
+"privacy-policy/index.html": "763bd168c5922aea9b77795ff9638bfe",
+"privacy-policy/privacy-policy.md": "d41d8cd98f00b204e9800998ecf8427e",
 "version.json": "7adbd5c29281b51f1722fb2e09be415f"
 };
 
