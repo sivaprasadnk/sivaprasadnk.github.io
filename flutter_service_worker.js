814e4096fd475cb2a790426e5690166c5f8403cc
sivaprasadnk <sivaprasadnk123@gmail.com>
2022-12-20 10:34:24 +0530

    adding pdf creator privacy policy

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -79,24 +79,28 @@ const RESOURCES = {
 "assets/assets/svg/wave.svg": "3d51290de60d7a2c910570f524050ef2",
 "assets/assets/svg/whatsapp_1.svg": "5042039fd0a06b7e17f062345977658f",
 "assets/FontManifest.json": "5514cfca37424a7b7712fc8ebe834d43",
-"assets/fonts/MaterialIcons-Regular.otf": "7e7a6cccddf6d7b20012a548461d5d81",
-"assets/NOTICES": "6c18dc42a42697a871c100bf89697bc8",
+"assets/fonts/MaterialIcons-Regular.otf": "95db9098c58fd6db106f1116bae85a0b",
+"assets/NOTICES": "0c9abff55a7ef36147a9103c83fe4130",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/packages/font_awesome_flutter/lib/fonts/fa-brands-400.ttf": "b37ae0f14cbc958316fac4635383b6e8",
 "assets/packages/font_awesome_flutter/lib/fonts/fa-regular-400.ttf": "5178af1d278432bec8fc830d50996d6f",
 "assets/packages/font_awesome_flutter/lib/fonts/fa-solid-900.ttf": "aa1ec80f1b30a51d64c72f669c1326a7",
-"canvaskit/canvaskit.js": "c2b4e5f3d7a3d82aed024e7249a78487",
-"canvaskit/canvaskit.wasm": "4b83d89d9fecbea8ca46f2f760c5a9ba",
-"canvaskit/profiling/canvaskit.js": "ae2949af4efc61d28a4a80fffa1db900",
-"canvaskit/profiling/canvaskit.wasm": "95e736ab31147d1b2c7b25f11d4c32cd",
+"assets/shaders/ink_sparkle.frag": "00ee8cf07e32f8b0588737cc6614c9f7",
+"canvaskit/canvaskit.js": "2bc454a691c631b07a9307ac4ca47797",
+"canvaskit/canvaskit.wasm": "bf50631470eb967688cca13ee181af62",
+"canvaskit/profiling/canvaskit.js": "38164e5a72bdad0faa4ce740c9b8e564",
+"canvaskit/profiling/canvaskit.wasm": "95a45378b69e77af5ed2bc72b2209b94",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "icons/Icon-192.png": "11cf43083c086a6822d0d71a849a78d7",
 "icons/Icon-192a.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "784370b76af581ab3e123c043b3734a6",
+"index.html": "04f65c032d47a17950d85d229a032f72",
 "/": "bb96f6fd4e74b29ff297d9748416de8b",
-"main.dart.js": "345eca65dd2da5e6f7b09b631ccb8e2a",
+"main.dart.js": "da6f3189de0b8ed9d35451e22c835f1f",
 "manifest.json": "c5f77348de3d65f8126c73b9bce93f8c",
+"pdf-creator/privacy-policy/index.html": "424d1b09f0af5d21e4dc0c1eb8e94ba7",
+"pdf-creator/privacy-policy/privacy-policy.md": "d41d8cd98f00b204e9800998ecf8427e",
 "privacy-policy/index.html": "bb96f6fd4e74b29ff297d9748416de8b",
 "privacy-policy/privacy-policy.md": "d41d8cd98f00b204e9800998ecf8427e",
 "version.json": "7adbd5c29281b51f1722fb2e09be415f"
@@ -105,10 +109,8 @@ const RESOURCES = {
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = [
-  "/",
-"main.dart.js",
+  "main.dart.js",
 "index.html",
-"assets/NOTICES",
 "assets/AssetManifest.json",
 "assets/FontManifest.json"];
 // During install, the TEMP cache is populated with the application shell files.
@@ -207,9 +209,11 @@ self.addEventListener("fetch", (event) => {
     .then((cache) =>  {
       return cache.match(event.request).then((response) => {
         // Either respond with the cached resource, or perform a fetch and
-        // lazily populate the cache.
+        // lazily populate the cache only if the resource was successfully fetched.
         return response || fetch(event.request).then((response) => {
-          cache.put(event.request, response.clone());
+          if (response && Boolean(response.ok)) {
+            cache.put(event.request, response.clone());
+          }
           return response;
         });
       })
