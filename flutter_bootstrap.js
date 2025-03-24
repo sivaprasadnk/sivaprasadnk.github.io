175c8e80daf39aeeb6fc1a8497c984b50a04dbc1
sivaprasadnk <sivaprasadnk123@gmail.com>
2025-03-24 14:23:30 +0530

    resume updated

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "757938208"
+    serviceWorkerVersion: "1803033598"
   }
 });
